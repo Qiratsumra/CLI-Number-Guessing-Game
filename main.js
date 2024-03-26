833c2a0003f45b5c0a7cbf403f8cb95d4f7548ff
Qiratsumra <saeedqirat43@gmail.com>
2024-03-26 11:37:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+console.log("\n Welcome to Qirat Saeed Number Guessing Game \n");
+let myRandomNumber = Math.floor(Math.random() * 15 + 1);
+let answer = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Select the number from 1-15",
+    }
+]);
+if (answer.userNumber === myRandomNumber) {
+    console.log("Congralutions you select the right number");
+}
+else {
+    console.log("OOP! you select the wrong number");
+}
